33ddfaef168ed9e98b4854ea9b544bf06ead1ab5
anantdevcs <anantdeviitism@gmail.com>
2020-05-21 23:33:22 +0530

    another try be successful

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 from flask import Flask
-
+import tensorflow as tf
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return 'Hello WOrld'
+    print("load started")
+    new_model = tf.keras.models.load_model('saved_model.h5')
+
+    return f'Hello WOrld {new_model.summary()}  dee '
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug =  True)
