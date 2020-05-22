f47c5cc4f9388865e165162e0b9c5a9d2be8e808
anantdevcs <anantdeviitism@gmail.com>
2020-05-22 13:55:06 +0530

    cww

--- application.py
+++ application.py
@@ -2,13 +2,29 @@ from flask import Flask
 #import tensorflow as tf
 app = Flask(__name__)
 
+import subprocess
+import sys
+
+
+
 @app.route('/')
 
 def index():
     #print("load started")
     #new_model = tf.keras.models.load_model('saved_model.h5')
 
-    return f'Hello WOrld  dee '
+    try:
+        import pandas as pd
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'pandas'])
+    finally:
+        import pandas as pd
+
+        
+
+    v = pd.__version__
+
+    return f'Hello WOrld  dee {v} '
 
 
 if __name__ == '__main__':
